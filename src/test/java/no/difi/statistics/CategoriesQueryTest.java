75687cf8ac467529c0816f445cd6fdce9dcf4b51
Claus Guttesen <kometen@gmail.com>
2022-09-19 15:09:40 +0200

    Add test to CategoriesQuery.

--- src/test/java/no/difi/statistics/CategoriesQueryTest.java
+++ src/test/java/no/difi/statistics/CategoriesQueryTest.java
@@ -1,11 +1,22 @@
 package no.difi.statistics;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import no.difi.statistics.elasticsearch.commands.CategoriesQuery;
+import no.difi.statistics.model.OwnerCategories;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CategoriesQueryTest {
 
@@ -34,5 +45,120 @@ public class CategoriesQueryTest {
         String distanceToken = "hour2022";
         String distance = categoriesQuery.determineDistance(distanceToken);
         assertEquals("hours", distance);
+    }
+
+    @Test
+    public void testKeyIsPresent() {
+        Path path = Paths.get("src/test/java/no/difi/statistics/CategoriesMappingInput.json");
+        String mappings = null;
+        try {
+            mappings = Files.lines(path).reduce("", String::concat);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = mapper.readTree(mappings);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        CategoriesQuery categoriesQuery = new CategoriesQuery();
+        Map<String, OwnerCategories> ownerCategoriesMap = categoriesQuery.traverseJsonNode(jsonNode);
+
+        assertFalse(ownerCategoriesMap.containsKey("991825827:foo-bar-baz:hours"));
+        assertFalse(ownerCategoriesMap.containsKey("123456789:idporten-innlogging:hours"));
+        assertFalse(ownerCategoriesMap.containsKey("991825827:idporten-innlogging:minutter"));
+
+        assertTrue(ownerCategoriesMap.containsKey("991825827:efmtest:hours"));
+        assertTrue(ownerCategoriesMap.containsKey("991825827:kontaktregister:hours"));
+        assertTrue(ownerCategoriesMap.containsKey("991825827:idporten-innlogging:hours"));
+    }
+
+    @Test
+    public void testCorrectLengthInSetIsReturned() {
+        Path path = Paths.get("src/test/java/no/difi/statistics/CategoriesMappingInput.json");
+        String mappings = null;
+        try {
+            mappings = Files.lines(path).reduce("", String::concat);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = mapper.readTree(mappings);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        CategoriesQuery categoriesQuery = new CategoriesQuery();
+        Map<String, OwnerCategories> ownerCategoriesMap = categoriesQuery.traverseJsonNode(jsonNode);
+
+        OwnerCategories efmtestCategories = ownerCategoriesMap.get("991825827:efmtest:hours");
+        assertEquals(5, efmtestCategories.getCategories().size());
+
+        OwnerCategories krrCategories = ownerCategoriesMap.get("991825827:kontaktregister:hours");
+        assertEquals(0, krrCategories.getCategories().size());
+
+        OwnerCategories idportenCategories = ownerCategoriesMap.get("991825827:idporten-innlogging:hours");
+        assertEquals(9, idportenCategories.getCategories().size());
+    }
+
+    @Test
+    public void testValidSetIsReturned() {
+        Path path = Paths.get("src/test/java/no/difi/statistics/CategoriesMappingInput.json");
+        String mappings = null;
+        try {
+            mappings = Files.lines(path).reduce("", String::concat);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = mapper.readTree(mappings);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        CategoriesQuery categoriesQuery = new CategoriesQuery();
+        Map<String, OwnerCategories> ownerCategoriesMap = categoriesQuery.traverseJsonNode(jsonNode);
+
+        OwnerCategories efmtestCategories = ownerCategoriesMap.get("991825827:efmtest:hours");
+        OwnerCategories krrCategories = ownerCategoriesMap.get("991825827:kontaktregister:hours");
+        OwnerCategories idportenCategories = ownerCategoriesMap.get("991825827:idporten-innlogging:hours");
+
+        Set<String> testSet = null;
+
+        testSet = Set.of("a", "b", "c");
+        assertFalse(efmtestCategories.getCategories().equals(testSet));
+
+        testSet = Set.of("service_identifier", "document_identifier", "process_identifier", "orgnr", "status");
+        assertTrue(efmtestCategories.getCategories().equals(testSet));
+
+        testSet = Set.of("a", "b", "c");
+        assertFalse(krrCategories.getCategories().equals(testSet));
+
+        testSet = Set.of();
+        assertTrue(krrCategories.getCategories().equals(testSet));
+
+        testSet = Set.of("d", "e", "f");
+        assertFalse(idportenCategories.getCategories().equals(testSet));
+
+        testSet = Set.of("TE-orgnum",
+                "TL-orgnum",
+                "TE",
+                "TL-entityId",
+                "TE-entityId",
+                "TL",
+                "Kategori 2/3",
+                "Kategori 3/3",
+                "Kategori 1/3");
+        assertTrue(idportenCategories.getCategories().equals(testSet));
     }
 }
