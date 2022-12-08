f762056f2e53c4db8f031aa82a020b267ef0bc2f
Erik Dahl <89081489+erkdahl@users.noreply.github.com>
2022-12-08 09:07:11 +0100

    Added prometheus tag (#47)

--- src/main/java/no/difi/statistics/UtdataAPI.java
+++ src/main/java/no/difi/statistics/UtdataAPI.java
@@ -1,7 +1,11 @@
 package no.difi.statistics;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
@@ -9,5 +13,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class UtdataAPI {
     public static void main(String[] args) {
         SpringApplication.run(UtdataAPI.class, args);
+    }
+
+    /**
+     * In order to tag prometheus metrics with application name.
+     */
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
     }
 }
